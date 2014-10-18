f50086fa65557569f649740e2421b2f5fd17aa05
helen <uhelen94@gmail.com>
2014-10-17 19:33:05 -0700

    PHP
    Made index page php instead

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>FindRhy.me</title>
+		<link href="index.css" type="text/css" rel="stylesheet" />
+		<link rel="icon" href="" />
+	</head>
+
+	<body>
+		<h1>FindRhy.me</h1>
+		<div id="map"></div>
+	</body>
+</html>
